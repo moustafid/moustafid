79110344a21949e4c9fa9335c4a9a8dc39ea64ef
Your Name <you@example.com>
2022-05-06 02:31:45 +0100

    first commit

---
+++ app/Http/Controllers/Admin/LoginController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\LoginRequest;
+use Illuminate\Http\Request;
+
+class LoginController extends Controller
+{
+    public function getLogin() {
+        return view('admin.auth.login');
+    }
+
+    public function login(LoginRequest $request) {
+        $remember_me = $request->has('remember_me') ? true : false;
+
+        if (auth()->guard('admin')->attempt(['email' => $request->input("email"), 'password' => $request->input("password")])) {
+            //notify()->success('تم الدخول بنجاح');
+            return redirect()->route('admin.dashboard');
+        }
+        //notify()->error('خطأ في البيانات برجاء المحاولة مجدداَ');
+        return redirect()->back()->with(['error' => ' هنالك خطأ بالبيانات']);
+
+    }
+}
